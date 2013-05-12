62ace05c8bcb4a1da3a32853ebbdadb2cff6c658
SHIMIZU Taku <shimizu.taku@gmail.com>
2013-05-13 08:28:56 +0900

    Change task function name

--- nodejs.py
+++ nodejs.py
@@ -4,4 +4,4 @@ from fabric.api import task
 
 from fabtools import require
 
-installed = task(require.nodejs.installed_from_source)
+installed_from_source = task(require.nodejs.installed_from_source)
