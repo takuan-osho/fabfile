67ac3d75063d2bac913fc834e718eaf895f3c9b6
SHIMIZU Taku <shimizu.taku@gmail.com>
2013-05-13 08:30:27 +0900

    Add functionality of setting up Debian based OS with Vagrant

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 # -*- coding:utf-8 -*-
 
 from fabric.api import task
+from fabric.api import local
 
 from fabtools.vagrant import vagrant
+from fabtools.vagrant import vagrant_settings
 
 from cuisine import package_update
 from cuisine import package_upgrade
@@ -16,3 +18,15 @@ from . import myconfig
 
 package_update = task(package_update)
 package_upgrade = task(package_upgrade)
+
+
+@task
+def setup_deb():
+    local('vagrant up')
+    with vagrant_settings():
+        package_update()
+        package_upgrade()
+
+        deb.setup_devtools()
+
+        python.setup_package_manager()
