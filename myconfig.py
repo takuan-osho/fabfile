ecb5b3fd5d2f5a14cd50c03fbb59e32e352ab875
SHIMIZU Taku <shimizu.taku@gmail.com>
2013-05-13 14:07:51 +0900

    Add idempotency-like feature to setup_dotfiles function

--- myconfig.py
+++ myconfig.py
@@ -4,13 +4,12 @@ from fabric.api import task
 from fabric.api import run
 from fabric.api import cd
 
-import fabtools
+from fabtools import files
 from fabtools.require import git
 
 import cuisine
 
 
-@task
 def install_dotfiles():
     git.working_copy('https://github.com/takuan-osho/dotfiles.git',
                      path='$HOME/dotfiles')
@@ -18,9 +17,11 @@ def install_dotfiles():
 
 @task
 def setup_dotfiles():
-    if fabtools.files.is_dir('$HOME/dotfiles'):
+    if files.is_dir('$HOME/dotfiles') and not files.is_dir('$HOME/.dotfiles'):
+        install_dotfiles()
         run('mv $HOME/dotfiles/ $HOME/.dotfiles')
-    with cd('.dotfiles'):
-        run('bash bash_setup.sh')
-    git.working_copy('https://github.com/Shougo/neobundle.vim.git',
-                     path='$HOME/.dotfiles/.vim/bundle/neobundle.vim')
+        with cd('$HOME/.dotfiles'):
+            run('bash bash_setup.sh')
+        with cd('$HOME/.dotfiles/.vim/bundle'):
+            run('rm -rf neobundle.vim')
+            git.working_copy('https://github.com/Shougo/neobundle.vim.git')
